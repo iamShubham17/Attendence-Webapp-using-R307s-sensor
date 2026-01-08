da881448e6c6db3054513e8c1d7a3594046de42c
Shubham Giradkar <116667932+iamShubham17@users.noreply.github.com>
2026-01-08 10:17:17 +0530

    Update WiFi credentials in FingerPrintSensor.ino

--- FingerPrintSensor/FingerPrintSensor.ino
+++ FingerPrintSensor/FingerPrintSensor.ino
@@ -15,8 +15,8 @@
 #include <ArduinoJson.h>
 
 // ==================== CONFIG ====================
-const char* ssid = "Happy Home";
-const char* password = "Giradkar@1717";
+const char* ssid = "DMIHER";
+const char* password = "jIO@1234";
 
 // Your wiring: TX(sensor) → D1, RX(sensor) → D2
 SoftwareSerial mySerial(5, 4);                    // RX = GPIO5 (D1), TX = GPIO4 (D2)
